6598fcf8f41973c27b539e07b46cce29b5d04e06
guerreroeileen <eileenguerrerogomez@gmail.com>
2024-08-14 22:24:06 -0500

    update to angular 14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import { EstadosService } from './services/estados/estados.service';
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild(MatSort) sort: MatSort;
 
 
-  personaForm: FormGroup;
+  personaForm: UntypedFormGroup;
   paises: any;
   estados: any;
   personas: any;
@@ -31,7 +31,7 @@ export class AppComponent implements OnInit, AfterViewInit {
 
 
   constructor(
-    public fb: FormBuilder,
+    public fb: UntypedFormBuilder,
     public estadosService: EstadosService,
     public paisesService: PaisesService,
     public personaService: PersonaService
